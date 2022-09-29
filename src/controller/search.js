1459dabb15b2df89bbcaf6c47aafbfd5e65cd423
熊熊熊子路 <1711279251@qq.com>
2022-09-30 00:08:20 +0800

    feat: 实现热搜关键字的获取

---
+++ src/controller/search.js
@@ -0,0 +1,44 @@
+const {createResponse} = require("../utils/createResponse");
+const {getTimeStamp} = require("../utils/getTimeStamp");
+const axios = require("axios");
+
+const getHotKeywords = async (ctx) => {
+    const cookie = ctx.headers.cookie;
+    const { session, token } = ctx.query;
+    try {
+        if (cookie && session && token){
+            const res = await axios.get('http://j.facerome.com/modules/article/toplist.php', {
+                headers: {
+                    "SESSIONID": session,
+                    "user-agent": 'Mozilla/5.0 (Linux; Android 12; M2006J10C Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/34.909092)',
+                    "Host": "j.facerome.com",
+                    "Connextion": "Keep-Alive",
+                    "Accept-Encoding": "gzip",
+                    "Cookie": cookie,
+                },
+                params: {
+                    order: 'postdata',
+                    sortid: 0,
+                    page: 1,
+                    time: await getTimeStamp(),
+                    "jieqi_token": token
+                }
+            })
+            if (res.data.data.articlerows.length > 0){
+                ctx.body = await createResponse(200, "获取成功", res.data.data.articlerows);
+            }else {
+                ctx.body = await createResponse(501, "系统错误请稍后重试", "");
+            }
+        }else {
+            ctx.body = await createResponse(500, "请重新登录账号");
+        }
+    }catch (e) {
+        throw e;
+    }
+}
+
+const getResult = async (ctx) => {
+
+}
+
+module.exports = { getHotKeywords, getResult }
