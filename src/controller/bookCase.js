225ea2d7b01ddc853abdf3ae52a3eb478a02f0f7
熊熊熊子路 <1711279251@qq.com>
2022-09-30 04:45:19 +0800

    feat: 完成编写 批量删除书架书籍接口 的 路由 和 控制器

--- src/controller/bookCase.js
+++ src/controller/bookCase.js
@@ -2,6 +2,30 @@ const {createResponse} = require("../utils/createResponse");
 const axios = require("axios");
 const {getTimeStamp} = require("../utils/getTimeStamp");
 
+const deleteOneBookFun = async (cookie, session, token, bid) => {
+    try {
+        const res = await axios.post('http://j.facerome.com/modules/article/bookcase.php',{
+            "bid": bid,
+            "act": "delete",
+            "time": await getTimeStamp(),
+            "jieqi_token": token
+        }, {
+            headers: {
+                "SESSIONID": session,
+                "user-agent": 'Mozilla/5.0 (Linux; Android 12; M2006J10C Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/34.909092)',
+                "Host": "j.facerome.com",
+                "Connection": "Keep-Alive",
+                "Accept-Encoding": "gzip",
+                "Cookie": cookie,
+                "Content-Type": "application/json"
+            }
+        })
+        return res
+    }catch (e) {
+        throw e;
+    }
+}
+
 const getBookCaseList = async (ctx) => {
     const cookie = ctx.headers.cookie;
     const { session, token } = ctx.query;
@@ -44,22 +68,7 @@ const deleteOneBook = async (ctx) => {
     try {
         if (cookie && session && token){
             if (bid){
-                const res = await axios.post('http://j.facerome.com/modules/article/bookcase.php',{
-                    "bid": bid,
-                    "act": "delete",
-                    "time": await getTimeStamp(),
-                    "jieqi_token": token
-                }, {
-                    headers: {
-                        "SESSIONID": session,
-                        "user-agent": 'Mozilla/5.0 (Linux; Android 12; M2006J10C Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/34.909092)',
-                        "Host": "j.facerome.com",
-                        "Connection": "Keep-Alive",
-                        "Accept-Encoding": "gzip",
-                        "Cookie": cookie,
-                        "Content-Type": "application/json"
-                    }
-                })
+                const res = await deleteOneBookFun(cookie, session ,token, bid);
                 if (res.data.code === 0 && res.data.data.redirect_url === '/modules/article/bookcase.php'){
                     ctx.body = await createResponse(200, "删除成功", "")
                 }else {
@@ -76,7 +85,41 @@ const deleteOneBook = async (ctx) => {
     }
 }
 
+const deleteMultiBooks = async (ctx) => {
+    const cookie = ctx.headers.cookie;
+    const { session, token, bids } = ctx.request.body;
+    try {
+        if (session && cookie && token){
+            if (bids !== ""){
+                // 解析 bids
+                const bidsArr = bids.split('|');
+                let isDelete = true;
+                let errNum = 0;
+                for (let i = 0; i < bidsArr.length; i++){
+                    const res = await deleteOneBookFun(cookie, session, token, bidsArr[i]);
+                    if (res.data.code !== 0 || res.data.data.redirect_url !== '/modules/article/bookcase.php'){
+                        isDelete = false;
+                        errNum ++;
+                    }
+                }
+                if (isDelete){
+                    ctx.body = await createResponse(200, "批量删除成功", "");
+                }else {
+                    ctx.body = await createResponse(502, `共需删除${bids.length}， 未删除${ errNum }`, "");
+                }
+            }else {
+                ctx.body = await createResponse(500, "请求错误，请稍后重试");
+            }
+        }else {
+            ctx.body = await createResponse(500, "请重新登录账号", "");
+        }
+    }catch (e) {
+        throw e;
+    }
+}
+
 module.exports = {
     getBookCaseList,
-    deleteOneBook
+    deleteOneBook,
+    deleteMultiBooks
 }
--- src/router/bookCase.js
+++ src/router/bookCase.js
@@ -1,10 +1,12 @@
 const Router = require('koa-router');
-const { getBookCaseList, deleteOneBook} = require('../controller/bookCase');
+const { getBookCaseList, deleteOneBook, deleteMultiBooks} = require('../controller/bookCase');
 const bookCaseRouter = new Router();
 
 // 获取书架列表
 bookCaseRouter.get('/list', getBookCaseList);
-// 删除暑假中的一本书
+// 删除书架中的一本书
 bookCaseRouter.post('/delete', deleteOneBook);
+// 批量删除书架中的书本
+bookCaseRouter.post('/delete/multi', deleteMultiBooks)
 
 module.exports = bookCaseRouter;
