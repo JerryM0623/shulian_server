a391ae5dc2ddd98ef3ac6c574abe0ed50084163b
熊熊熊子路 <1711279251@qq.com>
2022-09-30 01:31:31 +0800

    feat: 完成编写 最近更新接口 的 路由 和 控制器

--- src/controller/indexPage.js
+++ src/controller/indexPage.js
@@ -1,5 +1,7 @@
 const { indexPageRequest } = require('../request/indexPageRequest');
 const {createResponse} = require("../utils/createResponse");
+const {getTimeStamp} = require("../utils/getTimeStamp");
+const axios = require("axios");
 
 const getSwiperList = async (ctx) => {
     const cookie = ctx.headers.cookie;
@@ -43,7 +45,43 @@ const getRecommendList = async (ctx) => {
     }
 }
 
+const getLastUpdate = async (ctx) => {
+    const cookie = ctx.headers.cookie;
+    const { session, token } = ctx.query;
+    try {
+        if (cookie && session && token){
+            const res = await axios.get('http://j.facerome.com/modules/article/toplist.php', {
+                headers: {
+                    "SESSIONID": session,
+                    "user-agent": 'Mozilla/5.0 (Linux; Android 12; M2006J10C Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/34.909092)',
+                    "Host": "j.facerome.com",
+                    "Connection": "Keep-Alive",
+                    "Accept-Encoding": "gzip",
+                    "Cookie": cookie,
+                },
+                params: {
+                    "order": "lastupdate",
+                    "sortid": "0",
+                    "page": "1",
+                    "time": await getTimeStamp(),
+                    "jieqi_token": token
+                }
+            })
+            if (res.data.data.articlerows.length > 0){
+                ctx.body = await createResponse(200, "获取成功", res.data.data.articlerows);
+            }else {
+                ctx.body = await createResponse(501, "系统错误请稍后重试", "");
+            }
+        }else {
+            ctx.body = await createResponse(500, "请重新登录账号", "");
+        }
+    }catch (e) {
+        throw e;
+    }
+}
+
 module.exports = {
     getSwiperList,
-    getRecommendList
+    getRecommendList,
+    getLastUpdate
 }
--- src/router/indexPage.js
+++ src/router/indexPage.js
@@ -1,10 +1,12 @@
 const Router = require('koa-router');
-const { getSwiperList, getRecommendList } = require('../controller/indexPage');
+const { getSwiperList, getRecommendList, getLastUpdate } = require('../controller/indexPage');
 const indexPageRouter = new Router();
 
 // 获取首页轮播图的信息
 indexPageRouter.get('/swiper', getSwiperList);
 // 潜力好书的信息
 indexPageRouter.get('/recommend', getRecommendList);
+// 最近更新的信息
+indexPageRouter.get('/lastupdate', getLastUpdate);
 
 module.exports = indexPageRouter;
