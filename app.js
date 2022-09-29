cb56e5ca6c66d3275aeb092fc4e7774c31478f7f
熊熊熊子路 <1711279251@qq.com>
2022-09-29 20:57:39 +0800

    feat: 启用 login 路由

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 
+// 引入分路由
+const loginRouter = require('./src/router/login');
+
+// 启用分路由
+router.use('/login', loginRouter.routes(), loginRouter.allowedMethods());
+
 // 启用 router
 app.use(router.routes()).use(router.allowedMethods());
 
+// 启用服务器
 app.listen(4477, () => {
     console.log('server init...');
     console.log(`this server is running at: http://localhost:4477`);
