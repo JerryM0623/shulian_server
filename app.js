55bd5a3bdf4c5242477d73a0823311b5bd065770
熊熊熊子路 <1711279251@qq.com>
2022-09-29 20:47:10 +0800

    feat: 初始化 koa-router

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
+
+// 启用 router
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4477, () => {
     console.log('server init...');
