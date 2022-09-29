c4262862c76fa24083a3123d3046b2e29bb71047
熊熊熊子路 <1711279251@qq.com>
2022-09-29 23:24:57 +0800

    feat: 专用于请求熟练服务器首页信息的函数编写完成

---
+++ src/request/indexPageRequest.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const {getTimeStamp} = require("../utils/getTimeStamp");
+
+const indexPageRequest = async (sessionId, token, cookie) => {
+    try{
+        const res = await axios.get('http://j.facerome.com/', {
+            headers:{
+                "SESSIONID": sessionId,
+                "user-agent": 'Mozilla/5.0 (Linux; Android 12; M2006J10C Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/34.909092)',
+                "Host": "j.facerome.com",
+                "Connection": "Keep-Alive",
+                "Accept-Encoding": "gzip",
+                "Cookie": cookie,
+            },
+            params:{
+                time: await getTimeStamp(),
+                "jieqi_token": token
+            }
+        })
+        return res;
+    }catch (e) {
+        throw e;
+    }
+}
+
+module.exports = { indexPageRequest }
