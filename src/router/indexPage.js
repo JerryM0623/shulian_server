0ac4766d4d2077f4d3f6f08554cc76001639ea8e
熊熊熊子路 <1711279251@qq.com>
2022-09-30 01:40:33 +0800

    feat: 完成编写 热门排行接口 和 完结小说接口 的 路由 和 控制器

--- src/router/indexPage.js
+++ src/router/indexPage.js
@@ -1,5 +1,10 @@
 const Router = require('koa-router');
-const { getSwiperList, getRecommendList, getLastUpdate } = require('../controller/indexPage');
+const { getSwiperList,
+    getRecommendList,
+    getLastUpdate,
+    getTopRanking,
+    getFinish
+} = require('../controller/indexPage');
 const indexPageRouter = new Router();
 
 // 获取首页轮播图的信息
@@ -8,5 +13,9 @@ indexPageRouter.get('/swiper', getSwiperList);
 indexPageRouter.get('/recommend', getRecommendList);
 // 最近更新的信息
 indexPageRouter.get('/lastupdate', getLastUpdate);
+// 热门排行的信息
+indexPageRouter.get('/topranking', getTopRanking);
+// 完结小说的信息
+indexPageRouter.get('/finish', getFinish);
 
 module.exports = indexPageRouter;
