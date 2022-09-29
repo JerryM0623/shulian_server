1db7b0734d7226bccda9db0e2c30969a56dd0fee
熊熊熊子路 <1711279251@qq.com>
2022-09-29 23:38:35 +0800

    feat: 编写轮精选书籍接口的路由与控制器完成

--- src/router/indexPage.js
+++ src/router/indexPage.js
@@ -1,8 +1,10 @@
 const Router = require('koa-router');
-const { getSwiperList } = require('../controller/indexPage');
+const { getSwiperList, getRecommendList } = require('../controller/indexPage');
 const indexPageRouter = new Router();
 
 // 获取首页轮播图的信息
 indexPageRouter.get('/swiper', getSwiperList);
+// 潜力好书的信息
+indexPageRouter.get('/recommend', getRecommendList);
 
 module.exports = indexPageRouter;
