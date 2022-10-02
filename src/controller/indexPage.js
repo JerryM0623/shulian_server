c89f404a808b298d6b3fa9710e9151fe68390350
熊熊熊子路 <1711279251@qq.com>
2022-10-03 00:58:27 +0800

    fix: 修正部分接口需要登录才能访问的bug

--- src/controller/indexPage.js
+++ src/controller/indexPage.js
@@ -4,45 +4,75 @@ const {getTimeStamp} = require("../utils/getTimeStamp");
 const axios = require("axios");
 
 const getSwiperList = async (ctx) => {
-    const cookie = ctx.headers.cookie;
-    const { session, token } = ctx.query;
-    try {
-        if (cookie && session && token){
-            // 请求官方服务器
-            const res = await indexPageRequest(session, token, cookie);
-            if (res.data.data["jieqi_pageblocks"]["22"].title){
-                ctx.body = await createResponse(200, "获取成功", res.data.data["jieqi_pageblocks"]["22"])
-            }else{
-                ctx.body = await createResponse(501, "系统错误请稍后重试", "");
-            }
-        }else {
-            ctx.body = await createResponse(500, "请重新登录账号", "");
+
+    try{
+        // 请求官方服务器
+        // const res = await indexPageRequest(session, token, cookie);
+        const res = await indexPageRequest();
+        if (res.data.data["jieqi_pageblocks"]["22"].title){
+            ctx.body = await createResponse(200, "获取成功", res.data.data["jieqi_pageblocks"]["22"])
+        }else{
+            ctx.body = await createResponse(501, "系统错误请稍后重试", "");
         }
     }catch (e) {
         throw e;
     }
+
+    // const cookie = ctx.headers.cookie;
+    // const { session, token } = ctx.query;
+    // try {
+    //     if (cookie && session && token){
+    //         // 请求官方服务器
+    //         const res = await indexPageRequest(session, token, cookie);
+    //         if (res.data.data["jieqi_pageblocks"]["22"].title){
+    //             ctx.body = await createResponse(200, "获取成功", res.data.data["jieqi_pageblocks"]["22"])
+    //         }else{
+    //             ctx.body = await createResponse(501, "系统错误请稍后重试", "");
+    //         }
+    //     }else {
+    //         ctx.body = await createResponse(500, "请重新登录账号", "");
+    //     }
+    // }catch (e) {
+    //     throw e;
+    // }
 }
 
 const getRecommendList = async (ctx) => {
-    const cookie = ctx.headers.cookie;
-    const { session, token } = ctx.query;
-    try {
-        if (cookie && session && token){
-            // 请求官方服务器
-            const res = await indexPageRequest(session, token, cookie);
-            if (res.data.data["jieqi_pageblocks"]["21"].title){
-                const data = res.data.data["jieqi_pageblocks"]["21"];
-                data.title = "潜力好书";
-                ctx.body = await createResponse(200, "获取成功", data);
-            }else{
-                ctx.body = await createResponse(501, "系统错误请稍后重试", "");
-            }
-        }else {
-            ctx.body = await createResponse(500, "请重新登录账号", "");
+
+    try{
+        // 请求官方服务器
+        const res = await indexPageRequest();
+        if (res.data.data["jieqi_pageblocks"]["21"].title){
+            const data = res.data.data["jieqi_pageblocks"]["21"];
+            data.title = "潜力好书";
+            ctx.body = await createResponse(200, "获取成功", data);
+        }else{
+            ctx.body = await createResponse(501, "系统错误请稍后重试", "");
         }
     }catch (e) {
         throw e;
     }
+
+
+    // const cookie = ctx.headers.cookie;
+    // const { session, token } = ctx.query;
+    // try {
+    //     if (cookie && session && token){
+    //         // 请求官方服务器
+    //         const res = await indexPageRequest(session, token, cookie);
+    //         if (res.data.data["jieqi_pageblocks"]["21"].title){
+    //             const data = res.data.data["jieqi_pageblocks"]["21"];
+    //             data.title = "潜力好书";
+    //             ctx.body = await createResponse(200, "获取成功", data);
+    //         }else{
+    //             ctx.body = await createResponse(501, "系统错误请稍后重试", "");
+    //         }
+    //     }else {
+    //         ctx.body = await createResponse(500, "请重新登录账号", "");
+    //     }
+    // }catch (e) {
+    //     throw e;
+    // }
 }
 
 const getLastUpdate = async (ctx) => {
